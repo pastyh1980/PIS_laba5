24ced452ecac9b9d64a0ee82e11a91cb43a13efa
alexey.pastukhov <pastyh1980@gmail.com>
2020-04-03 11:25:53 +0300

    Search and minor fixes

--- Laba1/Controllers/HomeController.cs
+++ Laba1/Controllers/HomeController.cs
@@ -8,16 +8,30 @@ using System.Data.Entity;
 
 namespace Laba1.Controllers
 {
+    /*Основной контроллер*/
     public class HomeController : Controller
     {
+        /*Создание объекта класса DbContext*/
         PersonContext db = new PersonContext();
-        public ActionResult Index()
+
+        /*Вывод телефонной книги и поиск по запросу*/
+        public ActionResult Index(string query)
         {
             IEnumerable<Person> persons = db.persons;
+            if (!String.IsNullOrEmpty(query))
+            {
+                ViewBag.QueryString = query;
+                /*Применение фильтра*/
+                persons = persons.Where(p => p.firstName.ToUpper().Contains(query.ToUpper())
+                                          || p.lastName.ToUpper().Contains(query.ToUpper())
+                                          || p.secondName.ToUpper().Contains(query.ToUpper())
+                                          || p.phone.ToUpper().Contains(query.ToUpper()));
+            }
             ViewBag.Persons = persons;
             return View();
         }
 
+        /*Запрос формы на редактирование записи телеофнной книги*/
         [HttpGet]
         public ActionResult Edit(int? id)
         {
@@ -31,17 +45,26 @@ namespace Laba1.Controllers
             return new HttpNotFoundResult();
         }
 
+        /*Изменения записи в БД после редактирования*/
         [HttpPost]
         public ActionResult Edit(Person person)
         {
-            if (person.id > 0)
+            ModelState["id"].Errors.Clear();
+            if (!ModelState.IsValid)
+            {
+                /*Возврат на форму редактирования, если есть ошибки заполнения*/
+                return View(person);
+            }
+            if (person.id > 0) /*Создание или редактирование*/
                 db.Entry(person).State = EntityState.Modified;
             else
                 db.Entry(person).State = EntityState.Added;
+            /*Коммит в БД*/
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
+        /*Удаление записи*/
         public ActionResult Delete(int id)
         {
             Person p = new Person { id = id };
--- Laba1/Models/Person.cs
+++ Laba1/Models/Person.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Laba1.Models
 {
+    /*Класс записи телефонной книги*/
     public class Person
     {
+        /*Уникальный номер*/
+        [HiddenInput(DisplayValue = false)]
         public int id { get; set; }
 
+        /*Фамилия*/
+        [Required(ErrorMessage = "Поле Фамилия должно быть заполнено")]
         public string lastName { get; set; }
 
+        /*Имя*/
+        [Required(ErrorMessage = "Поле Имя должно быть заполнено")]
         public string firstName { get; set; }
 
+        /*Отчество*/
         public string secondName { get; set; }
 
+        /*Номер телефона*/
+        [Required(ErrorMessage = "Поле Телефон должно быть заполнено")]
         public string phone { get; set; }
 
         public Person() {}
